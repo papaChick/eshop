7fd6300926ce7d29fa6e5c085bc339eecbd0716e
papaChick <raphaelheryanto270605@gmail.com>
2025-02-20 02:01:18 +0700

    Fix: private constructor

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EshopApplication {
 
+	private EshopApplication() {
+		throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
-
 }
