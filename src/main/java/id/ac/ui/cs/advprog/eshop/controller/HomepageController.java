6fdd29e7b2d37ea78c7bfa368e19fc7cf81ae041
papaChick <raphaelheryanto270605@gmail.com>
2025-02-20 01:53:03 +0700

    Fix: Remove unused import

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/HomepageController.java
@@ -2,7 +2,8 @@ package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
