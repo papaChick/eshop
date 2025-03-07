730deb3c01f6adf591bd6d916b9194fdc13ee0df
papaChick <raphaelheryanto270605@gmail.com>
2025-03-07 20:00:49 +0700

    [GREEN] Implements Payment Repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,21 +1,46 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
     private List<Payment> paymentsData = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentsData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentsData.remove(i);
+                paymentsData.add(i, payment);
+                return payment;
+            }
+            i += 1;
+        }
+
+        paymentsData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment savedPayment : paymentsData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
     }
 
     public List<Payment> findAll() {
+        List<Payment> result = new ArrayList<>();
+        for (Payment payment : paymentsData) {
+            result.add(payment);
+        }
+        return result;
     }
 }
